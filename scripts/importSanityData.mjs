2cef223e1742ec13c4362a3bbc3a4c49d926faef
Mehak-Akram <akramabbasi019@gmail.com>
2025-01-22 07:48:26 +0500

    product updating

--- scripts/importSanityData.mjs
+++ scripts/importSanityData.mjs
@@ -1,3 +1,13 @@
+import { createClient } from '@sanity/client';
+
+const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, 
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,     
+  apiVersion: '2023-01-01',                            
+  useCdn: true,
+  token: process.env.SANITY_API_TOKEN,
+});
+
 async function uploadImageToSanity(imageUrl) {
   try {
     console.log(`Uploading image: ${imageUrl}`);
