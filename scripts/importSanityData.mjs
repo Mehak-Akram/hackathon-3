7b4b4bfd605dd735cb441f9e1effdfa89019deee
Mehak-Akram <akramabbasi019@gmail.com>
2025-01-22 09:00:29 +0500

    product update

--- scripts/importSanityData.mjs
+++ scripts/importSanityData.mjs
@@ -2,9 +2,9 @@ import { createClient } from '@sanity/client';
 
 const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, 
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,     
-  apiVersion: '2025-01-13',                            
-  useCdn: false,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: '2025-01-17',                            
+  useCdn: true,
   token: process.env.SANITY_API_TOKEN,
 });
 
--- src/components/recentProduct.tsx
+++ src/components/recentProduct.tsx
@@ -30,7 +30,7 @@ interface Product {
 export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, 
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,     
-  apiVersion: '2025-01-13',                            
+  apiVersion: '2025-01-17',                            
   useCdn: true, 
   token: process.env.SANITY_API_TOKEN,
 
